a4cfffcad0e47214204746e54ec484e989275bd5
Your Name <you@example.com>
2023-04-24 20:20:08 -0300

    a

--- next.config.js
+++ next.config.js
@@ -1,2 +1,7 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  exclude: [/\.jpg$/, /\.jpeg$/, /\.png$/, /\.gif$/, /\.svg$/],
+}
 
+module.exports = nextConfig
