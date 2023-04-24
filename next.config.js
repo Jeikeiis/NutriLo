6c3c40250b3a9b7ecde93981a8ff1454958c67c9
Jeikeiis <131185654+Jeikeiis@users.noreply.github.com>
2023-04-24 20:35:32 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  images: false,
-  exclude: [/\.jpg$/, /\.jpeg$/, /\.png$/, /\.gif$/, /\.svg$/],
+reactStrictMode: true,
+exclude: [/\.jpg$/, /\.jpeg$/, /\.png$/, /\.gif$/, /\.svg$/],
 }
 
 module.exports = nextConfig
