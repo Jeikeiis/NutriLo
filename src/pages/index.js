8c2eaf6e68d28aeada9c0aaae81085d47fad1aa8
Your Name <you@example.com>
2023-05-03 16:38:04 -0300

    A

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,12 @@ function IndexPage() {
       </head>
       <div class="flex">
       <div class="header"><br />
-      <div class="introheader1"><fieldset><p><strong>Bienvenidos a nuestra página de nutrición, donde promovemos un estilo de vida saludable a través de la alimentación balanceada y la actividad física.</strong><br/> <br/> En este sitio, encontrarás información detallada sobre nutrición, recetas saludables y consejos para mejorar tu bienestar general. Nuestro enfoque se basa en la evidencia científica y nuestro objetivo es ayudarte a alcanzar tus metas de salud y bienestar a largo plazo.
+      <div class="introheader1">
+      </div>
+      <div class="main">
+      <fieldset><p><strong>Bienvenidos a nuestra página de nutrición, donde promovemos un estilo de vida saludable a través de la alimentación balanceada y la actividad física.</strong><br/> <br/> En este sitio, encontrarás información detallada sobre nutrición, recetas saludables y consejos para mejorar tu bienestar general. Nuestro enfoque se basa en la evidencia científica y nuestro objetivo es ayudarte a alcanzar tus metas de salud y bienestar a largo plazo.
   
   Te invitamos a explorar nuestro sitio y descubrir más sobre nuestro equipo de expertos en nutrición, nuestros servicios y filosofía de trabajo.<br/> <br/> Puedes navegar fácilmente a través de nuestro menú de navegación, el cual incluye tres opciones principales: "Sobre nosotros", "Recetario" e "Inicio". Si buscas aprender más sobre nuestro equipo y filosofía, haz clic en "Sobre nosotros". Si quieres encontrar recetas saludables y fáciles de preparar, visita nuestro "Recetario". Si estás listo para empezar tu viaje hacia una vida más saludable, dirígete a "Inicio". ¡Gracias por visitarnos!</p></fieldset>
-      </div>
-      <div calss="main">
     </div>
     </div>
     </div><br/>
